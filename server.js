e088f0f89f9aba707aa8c323a33af33ec9e04943
Ongardiones <laureanonahuelromero18@gmail.com>
2025-04-11 23:03:40 -0300

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import deleteUser from './deleteUser.js';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Importar el middleware de CORS
 
 // Cargar variables de entorno desde .env
 dotenv.config();
 
 // Crear servidor
 const app = express();
+
+// Configurar CORS
+app.use(cors({
+    origin: 'http://127.0.0.1:5500', // Permitir solicitudes desde este origen
+    methods: ['POST', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type'], // Encabezados permitidos
+}));
 
 // Permite leer JSON en las solicitudes
 app.use(express.json());
